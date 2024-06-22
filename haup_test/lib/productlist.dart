ccf5fdd6223a0f899cae0ec0a1af00a4206b2ccd
NakarinFIgo <wangkit.nakarin@gmail.com>
2024-06-22 15:51:23 +0700

    feat : add page

---
+++ haup_test/lib/productlist.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Productlist extends StatefulWidget {
+  const Productlist({super.key});
+
+  @override
+  State<Productlist> createState() => _ProductlistState();
+}
+
+class _ProductlistState extends State<Productlist> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
